2aca20edf2469646fdfbcdc3909f17e45b3734b2
stormouse <stormouse@126.com>
2017-04-03 22:45:14 +0800

    tets

--- resources/views/admin/book_request/index.blade.php
+++ resources/views/admin/book_request/index.blade.php
@@ -31,7 +31,7 @@
                 @foreach($bookreqs as $bookreq)
                     <tr>
                         <td>{{$bookreq->user->username}}</td>
-                        <td>{{$bookreq->book->name}}</td>
+                        
                         <td>{{$bookreq->status==0?"待审核":($bookreq->status==1?"通过":"未通过")}}</td>
                         <td>{{mb_strlen(json_decode($bookreq->message)->remarks)>30?mb_substr(json_decode($bookreq->message)->remarks, 0, 27)."...":json_decode($bookreq->message)->remarks}}</td>
                         <td>
