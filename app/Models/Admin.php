0ed106a8d4d195101bd03806100993123fdc2247
stormouse <stormouse@126.com>
2017-04-08 19:16:12 +0800

    fix admin_table

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -7,7 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     protected $table = 'admin';
-	protected $fillable = ['role', 'user_id', 'department', 'district_id'];
-
-    
+	protected $fillable = ['role', 'user_id', 'department_id', 'district_id'];
 }
