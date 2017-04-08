35d4cabf547ba9b1159c22bf2816df35772852e0
stormouse <stormouse@126.com>
2017-04-08 22:23:15 +0800

    open to manually set weight

--- app/Helpers/RecommendHelper.php
+++ app/Helpers/RecommendHelper.php
@@ -22,7 +22,8 @@ class RecommendHelper {
     }
 
     public static function getHotBooks($limit=4){
-        return self::getBookRecommend(NULL, $limit);
+        $books = Book::orderBy('weight', 'desc')->limit($limit)->get();
+        return $books;
     }
 
     public static function getBookRecommend($user, $limit=10){
