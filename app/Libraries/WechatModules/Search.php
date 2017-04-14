757d6d92e6c553275d41eca8f9be9d72bbfe4180
Litaya <Litaya_San@163.com>
2017-04-14 17:24:18 +0800

    小改动

--- app/Libraries/WechatModules/Search.php
+++ app/Libraries/WechatModules/Search.php
@@ -6,6 +6,8 @@ use App\Models\Book;
 use App\Models\Material;
 use EasyWeChat\Message\News;
 use Illuminate\Support\Facades\Log;
+use Fukuball\Jieba\Jieba;
+use Fukuball\Jieba\Finalseg;
 
 class Search extends WechatHandler {
 
@@ -14,37 +16,45 @@ class Search extends WechatHandler {
 		$msg_type = $this->message->MsgType;
 		$openid  = $this->message->FromUserName;
 		if($msg_type == 'text'){
+
 			$search_msg  = $this->message->Content;
+			#$seg_list =  Jieba::cut($search_msg);
+			#$search_msg = "";
+			#foreach($seg_list as $seg){
+			#	$search_msg = $search_msg." ".$seg;
+			#}
+
+			$search_msg = trim($search_msg);
 			$book_results = Book::search($search_msg)->orderBy('weight','desc')->orderBy('publish_time','desc')->get();
 			if(sizeof($book_results)!=0){
 				$book_news = [];
 				foreach ($book_results as $book_result){
 					if(sizeof($book_news) < 5){
 						array_push($book_news,new News([
-							'title'       => substr($book_result->name."|".$book_result->authors,0,100),
-							'description' => '',
-							'url'         => route('book.show',$book_result->id),
-							'image'       => empty($book_result->img_upload)?route('image',['src'=>'public/book.png']):$book_result->img_upload
+									'title'       => substr($book_result->name."|".$book_result->authors,0,100),
+									'description' => '',
+									'url'         => route('book.show',$book_result->id),
+									'image'       => empty($book_result->img_upload)?route('image',['src'=>'public/book.png']):$book_result->img_upload
 						]));
 					}else
 						break;
 				}
 				$book_new = new News([
-					'title'       => "共查询到".sizeof($book_results)."本相关图书",
-					'description' => "点此查看相关图书列表",
-					'url'         => route('book.index')."?search=".$search_msg."&openid=$openid",
-					'image'       => route('image',['src'=>'public/book.png'])
+						'title'       => "共查询到".sizeof($book_results)."本相关图书",
+						'description' => "点此查看相关图书列表",
+						'url'         => route('book.index')."?search=".$search_msg."&openid=$openid",
+						'image'       => route('image',['src'=>'public/book.png'])
 				]);
 				array_push($book_news,$book_new);
-                Log::info('处理模块: Search');
+				Log::info('处理模块: Search');
 				return $book_news;
 			}
 		}
 		if(!empty($this->successor)){
-            Log::info('模块['.$this->name().']无法处理，传递给下一个模块');
+			Log::info('模块['.$this->name().']无法处理，传递给下一个模块');
 			return $this->successor->handle();
 		}else{
-            Log::info('模块['.$this->name().']是最后一个模块');
+			Log::info('模块['.$this->name().']是最后一个模块');
 			return "";
 		}
 	}
