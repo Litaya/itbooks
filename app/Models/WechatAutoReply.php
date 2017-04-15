4044b25d9cf02117b9eca6db6316374ed9183d8d
Litaya <Litaya_San@163.com>
2017-04-16 03:37:36 +0800

    微信后台功能：自定义回复的管理、微信功能的管理， TODO: 功能模块权重管理、每个模块处理之后触发次数的改变

--- app/Models/WechatAutoReply.php
+++ app/Models/WechatAutoReply.php
@@ -10,8 +10,9 @@ class WechatAutoReply extends Model
 
     protected $fillable = [
     	'id',
-	    'regex',  # 要匹配的正则表达式
-	    'type',   # 要回复的类型 0: 文字, 1: 图片, 2: 图文
-	    'content' # 如果是文字，存储文字内容； 如果是图片，存储图片地址； 如果是回复图文消息，则此处存储json数组，存储图文消息的id， [1,2]；
+	    'regex',   # 要匹配的正则表达式
+	    'type',    # 要回复的类型 0: 文字, 1: 图片, 2: 图文
+	    'content', # 如果是文字，存储文字内容； 如果是图片，存储图片地址； 如果是回复图文消息，则此处存储json数组，存储图文消息的id， [1,2]；
+	    'trigger_quantity' # 触发次数
     ];
 }
--- database/migrations/2017_04_08_181009_create_wechat_auto_reply_table.php
+++ database/migrations/2017_04_08_181009_create_wechat_auto_reply_table.php
@@ -18,6 +18,7 @@ class CreateWechatAutoReplyTable extends Migration
         	$table->string('regex')->comment('要匹配的正则表达式');
         	$table->integer('type')->comment('要回复的类型 0: 文字, 1: 图片, 2: 图文');
         	$table->string('content',1024)->default("")->comment('如果是文字，存储文字内容； 如果是图片，存储图片地址； 如果是回复图文消息，则此处存储json数组，存储图文消息的id， [1,2]；');
+        	$table->integer('trigger_quantity')->default(0);
         	$table->timestamps();
         });
     }
