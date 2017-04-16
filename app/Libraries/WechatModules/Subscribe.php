7d420d03b42843fb7beb75b526709c2fb47e561e
Litaya <Litaya_San@163.com>
2017-04-16 10:56:11 +0800

    修改关注时的回复消息

--- app/Libraries/WechatModules/Subscribe.php
+++ app/Libraries/WechatModules/Subscribe.php
@@ -38,8 +38,8 @@ class Subscribe extends WechatHandler {
 				} else {
 					User::where('openid', $open_id)->update(['subscribed' => 1]);
 				}
-				$reply = "感谢你的关注！我们会把最精彩的内容第一时间发给你！如果你是第一次光临，请先注册我们的会员，你将能够获得我们更多的服务。<br/>" .
-					"<a href='http://www.itshuquan.com/userinfo/basic?openid=" . $open_id . "'>用户注册</a><br/>" .
+				$reply = "感谢你的关注！我们会把最精彩的内容第一时间发给你！如果你是第一次光临，请先注册我们的会员，你将能够获得我们更多的服务\n" .
+					"<a href='http://www.itshuquan.com/userinfo/basic?openid=" . $open_id . "'>用户注册</a>\n" .
 					"<a href='https://itbook.kuaizhan.com/39/60/p332015340738c5'>新手指南</a>";
                 Log::info("处理模块: Subscribe");
 				return $reply;
