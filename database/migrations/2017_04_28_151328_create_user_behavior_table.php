151902ac6d94c742ff0c72e63ee658bb254891c9
Litaya <Litaya_San@163.com>
2017-04-30 22:08:46 +0800

    课件密码

--- database/migrations/2017_04_28_151328_create_user_behavior_table.php
+++ database/migrations/2017_04_28_151328_create_user_behavior_table.php
@@ -17,8 +17,8 @@ class CreateUserBehaviorTable extends Migration
         	$table->increments('id');
         	$table->string('module');
         	$table->unsignedInteger('user_id');
-        	$table->text('user_info');
         	$table->string('source')->comment('来自微信对话框/网页端行为');
+        	$table->string('uri')->comment('请求的uri');
         });
     }
 
