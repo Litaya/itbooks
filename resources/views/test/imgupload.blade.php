a27c01b73b6d7cdcff1e74f0e17e25ffc5c9ea06
Litaya <Litaya_San@163.com>
2017-05-06 22:48:26 +0800

    修复bug

--- resources/views/test/imgupload.blade.php
+++ resources/views/test/imgupload.blade.php
@@ -1,32 +1,36 @@
-<form action="{{ route('test.saveimage') }}" method="post">
-    {{ csrf_field() }}
-    <input type="text" name="img_media_id" id="img_media_id" hidden>
-</form>
+@extends('layouts.frame')
+@section('测试微信图片上传')
 
-<button class="btn btn-default" id="img_upload" onclick="choosImage()">点此选择要上传的图片</button>
+    <form action="{{ route('test.saveimage') }}" method="post">
+        {{ csrf_field() }}
+        <input type="text" name="img_media_id" id="img_media_id" hidden>
+    </form>
 
-<script>
-    wx.config(<?php echo $js->array('chooseImage', 'uploadImage'), true?>);
+    <button class="btn btn-default" id="img_upload" onclick="choosImage()">点此选择要上传的图片</button>
 
-    function choosImage(){
-        wx.chooseImage({
-            count: 1, // 默认9
-            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-            success: function (res) {
-                var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                wx.uploadImage({
-                    localId:localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
-                    isShowProgressTips: 1, // 默认为1，显示进度提示
-                    success: function (res) {
-                        var serverId = res.serverId; // 返回图片的服务器端ID
-                        $("#img_media_id").attr('value',serverId);
-                        alert(serverId);
-                    }
-                });
-            }
-        });
-    }
+    <script>
+        wx.config(<?php echo $js->config(array('chooseImage', 'uploadImage'), true)?>);
 
-</script>
+        function choosImage(){
+            wx.chooseImage({
+                count: 1, // 默认9
+                sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                success: function (res) {
+                    var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                    wx.uploadImage({
+                        localId:localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
+                        isShowProgressTips: 1, // 默认为1，显示进度提示
+                        success: function (res) {
+                            var serverId = res.serverId; // 返回图片的服务器端ID
+                            $("#img_media_id").attr('value',serverId);
+                            alert(serverId);
+                        }
+                    });
+                }
+            });
+        }
 
+    </script>
+
+@stop
