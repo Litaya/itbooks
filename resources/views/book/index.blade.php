429d23022424a90b3874d9f1351b616e0ade7142
Litaya <Litaya_San@163.com>
2017-07-30 11:18:23 +0800

    细节修改:1.用户样书申请页地址提示改为醒目的红色; 2.管理员端身份认证页面用户单位信息显示15字

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -10,11 +10,11 @@
             margin: 0;
         }
 
-        .col-center-block {  
-            float: none;  
-            display: block;  
-            margin-left: auto;  
-            margin-right: auto;  
+        .col-center-block {
+            float: none;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
         }
 
         .img-in-well {
@@ -34,112 +34,98 @@
 
     <div class="container-fluid">
 
-    <div class="row" style="margin-left: -20px; margin-right: -20px;">
-    {!! Form::open(["route"=>"book.index", "method"=>"GET"]) !!}
-    <div class="col-xs-9 col-md-9" style="padding: 0; margin: 0;">
-    {{ Form::text('search', null, ['placeholder'=>'发现更多好书', "class"=>"form-control", "style"=>"margin: 0;"]) }}
-    </div>
-    <div class="col-xs-3 col-md-3" style="padding: 0; margin: 0;">
-    {{ Form::submit('搜索', ["class"=>"form-control", "style"=>"margin: 0;"]) }}
-    </div>
-    </div>
-    {!! Form::close() !!}
+        <div class="row" style="margin-left: -20px; margin-right: -20px;">
+            {!! Form::open(["route"=>"book.index", "method"=>"GET"]) !!}
+            <div class="col-xs-9 col-md-9" style="padding: 0; margin: 0;">
+                {{ Form::text('search', null, ['placeholder'=>'发现更多好书', "class"=>"form-control", "style"=>"margin: 0;"]) }}
+            </div>
+            <div class="col-xs-3 col-md-3" style="padding: 0; margin: 0;">
+                {{ Form::submit('搜索', ["class"=>"form-control", "style"=>"margin: 0;"]) }}
+            </div>
+        </div>
+        {!! Form::close() !!}
     </div>
 
     <div class="row marginless">
 
-    <div class="col-xs-12">
-        <div class="panel panel-default">
-        
-        @if(count($topbooks) > 0)
-        <div class="panel-body" style="padding-left: 0px; padding-right: 0px;">
-            <div class="col-xs-5">
-                <a href="{{route('book.show', $topbooks[0]->id)}}"><img class="img-thumbnail img-in-well col-center-block" src="{{$topbooks[0]->img_upload}}" alt="编辑推荐"></a>
-            </div>
+        <div class="col-xs-12">
+            <div class="panel panel-default">
 
-            <div class="col-xs-7">
-                <small>
-                <p><strong>{{$topbooks[0]->name}}</strong></p>
-                作者: {{$topbooks[0]->authors}}<br>
-                定价: {{$topbooks[0]->price}}<br>
-                <br>
-                <p><small>{{$topbooks[0]->description}}</small></p>
-                </small>
-            </div>
-        </div>
-        @endif
+                @if(count($topbooks) > 0)
+                    <div class="panel-body" style="padding-left: 0px; padding-right: 0px;">
+                        <div class="col-xs-5">
+                            <a href="{{route('book.show', $topbooks[0]->id)}}"><img class="img-thumbnail img-in-well col-center-block" src="{{$topbooks[0]->img_upload}}" alt="编辑推荐"></a>
+                        </div>
 
-        <ul class="list-group">
-            <a data-toggle="collapse" href="#recommendCollapse" class="list-group-item">为您精选</a>
-            <div class="collapse" id="recommendCollapse">
-                <div class="well">
-                        <div class="row">
-                            @foreach($booksrecommend as $book)
-                            <div class="col-xs-6" style="margin-left: 0; margin-right: 0;">
-                            <div class="well well-showcase">
-                            <a href="{{route('book.show', $book->id)}}"><img class="img-thumbnail img-in-well col-center-block" src="{{$book->img_upload}}" alt="Book0"></a>
-                            <p>
-                                <h2>{{$book->name}}</h2>
-                            </p>
-                            
-                            </div>
-                            </div>
-                            @endforeach
+                        <div class="col-xs-7">
+                            <small>
+                                <p><strong>{{$topbooks[0]->name}}</strong></p>
+                                作者: {{$topbooks[0]->authors}}<br>
+                                定价: {{$topbooks[0]->price}}<br>
+                                <br>
+                                <p><small>{{$topbooks[0]->description}}</small></p>
+                            </small>
                         </div>
-                </div>
-            </div>
-            <a data-toggle="collapse" href="#newbookCollapse" class="list-group-item">最新上架</a>
-            <div class="collapse" id="newbookCollapse">
-                <div class="well">
-                        <div class="row">    
-                            @foreach($newbooks as $book)
-                            <div class="col-xs-6">
-                            <div class="well well-showcase">
-                            <a href="{{route('book.show', $book->id)}}"><img class="img-thumbnail img-in-well col-center-block" src="{{$book->img_upload}}" alt="Book0"></a>
-                            <p>
-                                <h2>{{$book->name}}</h2>
-                            </p>
-                            
-                            </div>
-                            </div>
-                            @endforeach
-                        </div>
-                </div>
-            </div>
-            <a data-toggle="collapse" href="#hotbookCollapse" class="list-group-item">热门图书</a>
-            <div class="collapse" id="hotbookCollapse">
-                <div class="well">
-                        <div class="row" id="hotbookRow">    
-                            @foreach($hotbooks as $book)
-                            <div class="col-xs-6">
-                            <div class="well well-showcase">
-                            <a href="{{route('book.show', $book->id)}}"><img class="img-thumbnail img-in-well col-center-block" src="{{$book->img_upload}}" alt="Book0"></a>
+                    </div>
+                @endif
 
-                            <p>
-                                <h2>{{$book->name}}</h2>
-                            </p>
-                            
+                <ul class="list-group">
+                    <a data-toggle="collapse" href="#recommendCollapse" class="list-group-item">为您精选</a>
+                    <div class="collapse" id="recommendCollapse">
+                        <div class="well">
+                            <div class="row">
+                                @foreach($booksrecommend as $book)
+                                    <div class="col-xs-6" style="margin-left: 0; margin-right: 0;">
+                                        <div class="well well-showcase">
+                                            <a href="{{route('book.show', $book->id)}}"><img class="img-thumbnail img-in-well col-center-block" src="{{$book->img_upload}}" alt="Book0"></a>
+                                            <p>
+                                            <h2>{{$book->name}}</h2>
+                                            </p>
+
+                                        </div>
+                                    </div>
+                                @endforeach
                             </div>
-                            </div>
-                            @endforeach
                         </div>
-                </div>
+                    </div>
+                    <a data-toggle="collapse" href="#newbookCollapse" class="list-group-item">最新上架</a>
+                    <div class="collapse" id="newbookCollapse">
+                        <div class="well">
+                            <div class="row">
+                                @foreach($newbooks as $book)
+                                    <div class="col-xs-6">
+                                        <div class="well well-showcase">
+                                            <a href="{{route('book.show', $book->id)}}"><img class="img-thumbnail img-in-well col-center-block" src="{{$book->img_upload}}" alt="Book0"></a>
+                                            <h2>{{$book->name}}</h2>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                    <a data-toggle="collapse" href="#hotbookCollapse" class="list-group-item">热门图书</a>
+                    <div class="collapse" id="hotbookCollapse">
+                        <div class="well">
+                            <div class="row" id="hotbookRow">
+                                @foreach($hotbooks as $book)
+                                    <div class="col-xs-6">
+                                        <div class="well well-showcase">
+                                            <a href="{{route('book.show', $book->id)}}"><img class="img-thumbnail img-in-well col-center-block" src="{{$book->img_upload}}" alt="Book0"></a>
+                                            <h2>{{$book->name}}</h2>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </ul>
             </div>
-        </ul>
-        </div>
         </div>
 
     </div>
 
+    <script>
 
-
-    
-    
-    </div>
-    </div>
-
-<script>
-
-</script>
+    </script>
 
 @endsection
