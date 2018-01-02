6708a83380effe0e87f9c4ecd280de671f502d2a
Litaya <Litaya_San@163.com>
2018-01-02 19:38:53 +0800

    导出全部样书申请单

--- app/Helpers/ScriptHelper.php
+++ app/Helpers/ScriptHelper.php
@@ -4,6 +4,9 @@ namespace App\Helpers;
 
 use App\Models\User;
 use App\Models\UserInfo;
+use Illuminate\Support\Facades\DB;
+
+use Excel;
 
 /**
  * Created by PhpStorm.
@@ -44,5 +47,76 @@ class ScriptHelper{
 			$user->save();
 		}
 		echo "共处理 $counter 条记录";
+	}
+
+	public static function exportAllBookRequest(){
+		$time = time();
+		DB::connection()->disableQueryLog();
+
+		$block_size = 2;
+		$block_index = 1;
+
+		while ($block_index){
+
+			if ($block_index % $block_size == 0){
+				echo $block_index % $block_size."\n";
+			}
+
+			$book_requests = DB::select('select book.isbn as isbn, book.name as bookname, book.price as bookprice,user.email as email,status, message, book_request.phone as bookreqphone, receiver, order_number, book_request.address as bookreqaddress, department.code as code, department.name as department_name from book_request left join book on book.id = book_request.book_id left join user on user.id = book_request.user_id left join department on department.id = book.department_id limit '.$block_size.' offset '.($block_size*($block_index-1)));
+			if (count($book_requests) == 0)
+				break;
+
+			$filename = date("Y-m-d")."样书申请单_".$time."_".$block_index;
+			$export = Excel::create($filename, function($excel) use ($book_requests){
+				$excel->sheet("样书申请单", function($sheet) use ($book_requests){
+
+					$sheet->setAutoSize(true);
+
+					$sheet->row(1, ["书代号", "书名", "定价", "常用邮箱", "申请状态","收货地址",
+						"收件人",'联系方式', "运单号",'部门代码','部门名称','教材使用情况','备注']);
+
+					foreach($book_requests as $book_request){
+						$sheet->appendRow([
+							$book_request["isbn"]." ",
+							$book_request["bookname"],
+							$book_request["bookprice"],
+							$book_request['email'],
+							$book_request['status'],
+							$book_request['bookreqaddress'],
+							$book_request['receiver'],
+							$book_request['bookreqphone'],
+							$book_request['order_number'],
+							$book_request['code'],
+							$book_request['department_name'],
+							json_decode($book_request['message'],true)['book_plan'],
+							json_decode($book_request['message'],true)['remarks'],
+						]);
+					}
+
+					$sheet->setColumnFormat(array(
+						'A' => '@',
+						'B' => '@',
+						'C' => '0.00',
+						'D' => '@',
+						'E' => '@',
+						'F' => '@',
+						'G' => '0',
+						'H' => '@',
+						'I' => '@',
+						'J' => '@',
+						'K' => '@',
+						'L' => '@',
+
+					));
+				});
+			})->store('xlsx');
+
+			$block_index++;
+
+			if (count($book_requests) < $block_size){
+				echo "共导出【".$block_index."】条记录";
+				break;
+			}
+		}
 	}
 }
