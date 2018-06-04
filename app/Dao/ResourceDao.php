43a0d32ac85d66aa114d3c2cd07235f3b779670d
Litaya <Litaya_San@163.com>
2018-06-04 21:44:41 +0800

    fix bug

--- app/Dao/ResourceDao.php
+++ app/Dao/ResourceDao.php
@@ -9,16 +9,22 @@
 namespace App\Dao;
 
 use App\Models\Resource;
+use App\Models\ResourceBook;
 use App\Models\User;
 
 class ResourceDao{
 
 	public function getAllResource(User $user, $book_id = null){
+
 		# 先获取每个人都可获取的资源
 		$common_resources = Resource::where('owner_book_id', 0)->get();
-
-		# 再获取对应书籍的所有资源
-		$book_resources = Resource::where('owner_book_id',$book_id)->get();
+        # 再获取对应书籍的所有资源
+        $resource_books = ResourceBook::where('book_id', $book_id)->get();
+        $book_resources = [];
+        foreach($resource_books as $rb){
+            $resource = Resource::where('id', $rb->resource_id)->first();
+            array_push($book_resources, $resource);
+        }
 
 		# 对于每个资源，检测其用户权限
 		$resources = array();
